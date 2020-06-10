b518f472542810faed220ce0895d09e77d660fb4
Aleck Zanderbier <aleckzts@gmail.com>
2020-06-10 13:07:09 -0300

    inicializacao das variaveis em Go

--- src/soma/soma.go
+++ src/soma/soma.go
@@ -2,14 +2,13 @@ package main
 import "fmt"
 
 func main() {
-    // var c int
-    var c := soma(5, 5)
+    c := soma(5, 5)
     fmt.Println("GO: soma de valores")
     fmt.Println("5 + 5 = %d", c)
 }
 
 func soma(a int, b int) int {
-  // var resultado int
-  var resultado := a + b
+  var resultado int
+  resultado := a + b
   return resultado
 }
